e8ae4d63faa1099c6cace40c56b66e230f93b2a8
Thomas Guignard <tom@timtom.ch>
2025-02-23 16:53:58 -0500

    Ajout de la fonction log

--- mesure_log.py
+++ mesure_log.py
@@ -4,6 +4,13 @@ import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+import logging
+
+# Définition du fichier de log
+logging.basicConfig(filename='humidite.log',level=logging.INFO,format='%(asctime)s %(message)s')
+
+# Intervalle de mesure (en secondes)
+intervalle = 30
 
 # Ouverture du bus I2C sur les pins SCL et SDA par défaut
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -23,21 +30,24 @@ senseur = AnalogIn(ads, ADS.P0)
 trop_sec = 2.7
 trop_humide = 2.0
 
+# Variable de statut
+statut_plante = 'ok'
+
 while True:
         humidite_mesuree = senseur.voltage
-        print(humidite_mesuree)
         
         if humidite_mesuree >= trop_sec:
-            print('Trop sec')
+            statut_plante = 'sec'
             verte.off()
             rouge.on()
         elif humidite_mesuree <= trop_humide:
-            print('Trop humide')
+            statut_plante = 'humide'
             verte.off()
             rouge.blink(0.2,2)
         else:
-            print('Parfait!')
+            statut_plante = 'ok'
             rouge.off()
             verte.on()
         
-        time.sleep(1)
+        logging.info("{:>5.4f}\t{:>5}".format(humidite_mesuree,statut_plante))
+        time.sleep(intervalle)
