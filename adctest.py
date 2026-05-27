94855c2aa23c3be5621b3eb9e6aa990c34a0b2d7
Thomas Guignard <tom@timtom.ch>
2026-05-27 19:11:20 -0400

    Fix ADCtest

--- adctest.py
+++ adctest.py
@@ -1,20 +1,19 @@
 import time
 import board
 import busio
-import adafruit_ads1x15.ads1015 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_ads1x15 import AnalogIn, ADS1015, ads1x15
 
 # Ouverture du bus I2C sur les pins SCL et SDA par défaut
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # Initialisation d'un objet ADC via le bus I2C
-ads = ADS.ADS1015(i2c)
+ads = ADS1015(i2c)
 
 # Initialisation des canaux
-canal0 = AnalogIn(ads, ADS.P0)
-canal1 = AnalogIn(ads, ADS.P1)
-canal2 = AnalogIn(ads, ADS.P2)
-canal3 = AnalogIn(ads, ADS.P3)
+canal0 = AnalogIn(ads, ads1x15.Pin.A0)
+canal1 = AnalogIn(ads, ads1x15.Pin.A1)
+canal2 = AnalogIn(ads, ads1x15.Pin.A2)
+canal3 = AnalogIn(ads, ads1x15.Pin.A3)
 
 print("{:>5}\t{:>5}\t{:>5}\t{:>5}".format('A0', 'A1', 'A2', 'A3'))
 
